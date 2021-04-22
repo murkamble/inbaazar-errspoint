5b32ed4f7c983521bab7ada18b9b0d021f7be1ab
cse-mkamble <76428798+cse-mkamble@users.noreply.github.com>
2021-04-22 08:45:55 +0530

    Fix JWT Expire login issue

--- client_admin/src/helpers/axios.js
+++ client_admin/src/helpers/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { api } from '../urlConfig';
+import { authConstants } from '../actions/constants';
+import store from "../store";
 
 const token = window.localStorage.getItem('token');
 
@@ -8,6 +10,25 @@ const axiosIntance = axios.create({
     headers:{
         'Authorization': token ? `Bearer ${token}` :''
     }
+});
+
+axiosIntance.interceptors.request.use((req) => {
+    const { auth } = store.getState();
+    if(auth.token){
+        req.headers.Authorization = `Bearer ${auth.token}`;
+    }
+    return req;
+});
+
+axiosIntance.interceptors.response.use((res) => {
+    return res;
+}, (error) => {
+    const { status } = error.response;
+    if (status === 500) {
+        localStorage.clear();
+        store.dispatch({ type: authConstants.LOGOUT_SUCCESS });
+    }
+    return Promise.reject(error);
 });
 
 export default axiosIntance;
